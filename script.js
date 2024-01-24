42b98474a918eed6603c480b668623af12d91822
wallisCodes <jwallis152@gmail.com>
2024-01-24 16:59:19 +0000

    Added contact page, tidied css (more to do), simplified js

---
+++ script.js
@@ -0,0 +1,14 @@
+function switchService(evt, serviceType) {
+  var i, x, service_tabs;
+  x = document.getElementsByClassName("services-list");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+
+  service_tabs = document.getElementsByClassName("service-tab");
+  for (i = 0; i < x.length; i++) {
+    service_tabs[i].className = service_tabs[i].className.replace(" active-tab", "");
+  }
+  document.getElementById(serviceType).style.display = "block";
+  evt.currentTarget.className += " active-tab";
+}
