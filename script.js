0247259e08f1c10813b00121194195a55dd19a15
wallisCodes <jwallis152@gmail.com>
2024-02-08 10:24:22 +0000

    Added js to highlight the current day on the opening times info

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-screen.orientation.lock("natural");
+screen.orientation.lock("natural"); // attemping to disable screen rotation (doesn't work)
 
+// Function to switch the display between the two service menus via the service tabs
 function switchService(evt, serviceType) {
   var i, x, service_tabs;
   x = document.getElementsByClassName("services-list");
@@ -14,3 +15,21 @@ function switchService(evt, serviceType) {
   document.getElementById(serviceType).style.display = "block";
   evt.currentTarget.className += " active-tab";
 }
+
+
+// Retrieving current week day to highlight it on the opening times info
+const date = new Date();
+day_index = date.getDay();
+
+var i, opening_times;
+opening_times = document.getElementsByClassName("opening-info");
+for (i = 0; i < opening_times.length; i++) {
+  opening_times[i].className = opening_times[i].className.replace(" current-day", "");
+}
+
+// workaround for mismatched indices (Sunday [0] in Date() object vs. Sunday [6] in opening_times object)
+if (day_index == 0) {
+  day_index = 7;
+}
+
+opening_times[day_index - 1].className += " current-day";
